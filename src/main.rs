99814cbf95dd57f67a41efb5cc0d56b7ddb2e326
Jesse Jafa <jesse.jafa@gmail.com>
2022-03-19 07:33:03 +0200

    add tests

--- src/main.rs
+++ src/main.rs
@@ -14,8 +14,8 @@ fn main() -> std::io::Result<()> {
 fn calculate_size(bytes: u128) -> String {
     const KB: u128 = 1024;
     const MB: u128 = u128::pow(KB, 2);
-    const GB: u128 = u128::pow(MB, 2);
-    const TB: u128 = u128::pow(GB, 2);
+    const GB: u128 = MB * KB;
+    const TB: u128 = GB * KB;
 
     if bytes < KB {
         return format!("{} B", bytes);
@@ -35,4 +35,49 @@ fn calculate_size(bytes: u128) -> String {
 
 fn remainder(bytes: u128, divisor: u128) -> u128 {
     bytes % divisor
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn bytes() {
+        let mut bytes = super::calculate_size(1024);
+        assert_eq!(bytes, "1,0 KB");
+
+        bytes = super::calculate_size(1023);
+        assert_eq!(bytes, "1023 B");
+    }
+
+    #[test]
+    fn kilobytes() {
+        let mut bytes = super::calculate_size(1024 * 1024);
+        assert_eq!(bytes, "1,0 MB");
+
+        bytes = super::calculate_size(1024 * 1023);
+        assert_eq!(bytes, "1023,0 KB");
+    }
+
+    #[test]
+    fn megabytes() {
+        let mut bytes = super::calculate_size(1024 * 1024 * 1024);
+        assert_eq!(bytes, "1,0 GB");
+
+        bytes = super::calculate_size(1024 * 1024 * 1023);
+        assert_eq!(bytes, "1023,0 MB");
+    }
+
+    #[test]
+    fn gigabytes() {
+        let mut bytes = super::calculate_size(1024 * 1024 * 1024 * 1024);
+        assert_eq!(bytes, "1,0 TB");
+
+        bytes = super::calculate_size(1024 * 1024 * 1024 * 1023);
+        assert_eq!(bytes, "1023,0 GB");
+    }
+
+    #[test]
+    fn terabytes() {
+        let bytes = super::calculate_size(1024 * 1024 * 1024 * 1024 * 1023);
+        assert_eq!(bytes, "1023,0 TB");
+    }
 }
