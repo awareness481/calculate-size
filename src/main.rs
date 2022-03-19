3ceb01d6a4b7098dd2e9f947a599902192a37c90
Jesse Jafa <jesse.jafa@gmail.com>
2022-03-19 07:39:34 +0200

    format remainder

--- src/main.rs
+++ src/main.rs
@@ -21,20 +21,29 @@ fn calculate_size(bytes: u128) -> String {
         return format!("{} B", bytes);
     }
     if bytes < MB {
-        return format!("{},{} KB", bytes / KB, remainder(bytes, KB));
+        return format!("{}{} KB", bytes / KB, format_remainder(bytes, KB));
     }
     if bytes < GB {
-        return format!("{},{} MB", bytes / MB, remainder(bytes, MB) / KB);
+        return format!("{}{} MB", bytes / MB, format_remainder(bytes, MB));
     }
     if bytes < TB {
-        return format!("{},{} GB", bytes / GB, remainder(bytes, GB) / MB);
+        return format!("{}{} GB", bytes / GB, format_remainder(bytes, GB));
     }
 
-    format!("{},{} TB", bytes / TB, remainder(bytes, TB) / GB)
+    format!("{}{} TB", bytes / TB, format_remainder(bytes, TB))
 }
 
 fn remainder(bytes: u128, divisor: u128) -> u128 {
     bytes % divisor
+}
+
+fn format_remainder(bytes: u128, divisor: u128) -> String {
+    let remainder = remainder(bytes, divisor);
+    let f = divisor / 1024;
+    if remainder / f == 0 {
+        return "".to_string();
+    }
+    format!(",{}", remainder)
 }
 
 #[cfg(test)]
@@ -42,7 +51,7 @@ mod tests {
     #[test]
     fn bytes() {
         let mut bytes = super::calculate_size(1024);
-        assert_eq!(bytes, "1,0 KB");
+        assert_eq!(bytes, "1 KB");
 
         bytes = super::calculate_size(1023);
         assert_eq!(bytes, "1023 B");
@@ -51,33 +60,33 @@ mod tests {
     #[test]
     fn kilobytes() {
         let mut bytes = super::calculate_size(1024 * 1024);
-        assert_eq!(bytes, "1,0 MB");
+        assert_eq!(bytes, "1 MB");
 
         bytes = super::calculate_size(1024 * 1023);
-        assert_eq!(bytes, "1023,0 KB");
+        assert_eq!(bytes, "1023 KB");
     }
 
     #[test]
     fn megabytes() {
         let mut bytes = super::calculate_size(1024 * 1024 * 1024);
-        assert_eq!(bytes, "1,0 GB");
+        assert_eq!(bytes, "1 GB");
 
         bytes = super::calculate_size(1024 * 1024 * 1023);
-        assert_eq!(bytes, "1023,0 MB");
+        assert_eq!(bytes, "1023 MB");
     }
 
     #[test]
     fn gigabytes() {
         let mut bytes = super::calculate_size(1024 * 1024 * 1024 * 1024);
-        assert_eq!(bytes, "1,0 TB");
+        assert_eq!(bytes, "1 TB");
 
         bytes = super::calculate_size(1024 * 1024 * 1024 * 1023);
-        assert_eq!(bytes, "1023,0 GB");
+        assert_eq!(bytes, "1023 GB");
     }
 
     #[test]
     fn terabytes() {
         let bytes = super::calculate_size(1024 * 1024 * 1024 * 1024 * 1023);
-        assert_eq!(bytes, "1023,0 TB");
+        assert_eq!(bytes, "1023 TB");
     }
 }
